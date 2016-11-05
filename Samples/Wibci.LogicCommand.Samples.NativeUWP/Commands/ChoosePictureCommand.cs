ba6d6d9f4ec8a341d5cd240ab606ba76b4d265b4
Malcolm Jack <inquisitorjax@gmail.com>
2016-11-05 15:55:56 +0200

    Add Native UWP sample app

---
+++ Samples/Wibci.LogicCommand.Samples.NativeUWP/Commands/ChoosePictureCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Wibci.LogicCommand.Samples.NativeUWP.Commands
+{
+    public interface IChoosePictureCommand : IAsyncLogicCommand<ChoosePictureContext, ChoosePictureResult>
+    {
+    }
+
+    public class ChoosePictureCommand : AsyncLogicCommand<ChoosePictureContext, ChoosePictureResult>, IChoosePictureCommand
+    {
+        public override async Task<ChoosePictureResult> ExecuteAsync(ChoosePictureContext request)
+        {
+            var retResult = new ChoosePictureResult();
+
+            var openPicker = new FileOpenPicker
+            {
+                ViewMode = request.ViewMode,
+                SuggestedStartLocation = request.SuggestedStartLocation
+            };
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".png");
+            var file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                retResult.File = file;
+            }
+            else
+            {
+                retResult.Notification.Add("File Empty", NotificationSeverity.Warning);
+            }
+
+            return retResult;
+        }
+    }
+
+    public class ChoosePictureContext
+    {
+        public ChoosePictureContext()
+        {
+            SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            ViewMode = PickerViewMode.Thumbnail;
+        }
+
+        public static ChoosePictureContext Default
+        {
+            get { return new ChoosePictureContext(); }
+        }
+
+        public PickerLocationId SuggestedStartLocation { get; set; }
+
+        public PickerViewMode ViewMode { get; set; }
+    }
+
+    public class ChoosePictureResult : CommandResult
+    {
+        public StorageFile File { get; set; }
+    }
+}
---
+++ Samples/Wibci.LogicCommand.Samples.NativeUWP/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Wibci.LogicCommand.Samples.NativeUWP.Commands;
+using Windows.UI.Popups;
+
+namespace Wibci.LogicCommand.Samples.NativeUWP
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public MainPageViewModel()
+        {
+            ChoosePictureCommand = DelegateCommand.FromAsyncHandler(ChoosePictureAsync);
+        }
+
+        public ICommand ChoosePictureCommand { get; }
+
+        private async Task ChoosePictureAsync()
+        {
+            var command = new ChoosePictureCommand();
+            var result = await command.ExecuteAsync(ChoosePictureContext.Default);
+
+            string message = "";
+
+            if (result.IsValid(NotificationSeverity.Warning) && result.File != null)
+            {
+                message = $"You picked file: {result.File.DisplayName}";
+            }
+            else
+            {
+                message = "No File Picked :(";
+            }
+            var dialog = new MessageDialog(message);
+            await dialog.ShowAsync();
+        }
+    }
+}
