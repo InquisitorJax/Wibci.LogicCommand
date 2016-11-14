6027e87ad15c9a41bbdd7ffa4726ccfd282c9ce5
Malcolm Jack <inquisitorjax@gmail.com>
2016-11-14 14:11:53 +0200

    Add TaskCommandResult

---
+++ Commands/TaskCommandResult.cs
@@ -0,0 +1,20 @@
+﻿namespace Wibci.LogicCommand
+{
+    public enum TaskResult
+    {
+        Success,
+        Failed,
+        AccessDenied,
+        Canceled
+    }
+
+    public class TaskCommandResult : CommandResult
+    {
+        public TaskCommandResult()
+        {
+            TaskResult = TaskResult.Success;
+        }
+
+        public TaskResult TaskResult { get; set; }
+    }
+}
