ca635980daaeabdef458cba8fa52d2a2fc5224aa
Malcolm Jack <inquisitorjax@gmail.com>
2018-01-06 11:21:44 +0200

    update sample

--- Samples/Wibci.LogicCommand.Samples.NativeUWP/MainPageViewModel.cs
+++ Samples/Wibci.LogicCommand.Samples.NativeUWP/MainPageViewModel.cs
@@ -1,39 +1,37 @@
-﻿using Prism.Commands;
+﻿using System.Windows.Input;
+using Prism.Commands;
 using Prism.Mvvm;
-using System;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using Wibci.LogicCommand.Samples.NativeUWP.Commands;
 using Windows.UI.Popups;
 
 namespace Wibci.LogicCommand.Samples.NativeUWP
 {
-    public class MainPageViewModel : BindableBase
-    {
-        public MainPageViewModel()
-        {
-            ChoosePictureCommand = DelegateCommand.FromAsyncHandler(ChoosePictureAsync);
-        }
+	public class MainPageViewModel : BindableBase
+	{
+		public MainPageViewModel()
+		{
+			ChoosePictureCommand = new DelegateCommand(ChoosePicture);
+		}
 
-        public ICommand ChoosePictureCommand { get; }
+		public ICommand ChoosePictureCommand { get; }
 
-        private async Task ChoosePictureAsync()
-        {
-            var command = new ChoosePictureCommand();
-            var result = await command.ExecuteAsync(ChoosePictureContext.Default);
+		private async void ChoosePicture()
+		{
+			var command = new ChoosePictureCommand();
+			var result = await command.ExecuteAsync(ChoosePictureContext.Default);
 
-            string message = "";
+			string message = "";
 
-            if (result.IsValid(NotificationSeverity.Warning) && result.File != null)
-            {
-                message = $"You picked file: {result.File.DisplayName}";
-            }
-            else
-            {
-                message = "No File Picked :(";
-            }
-            var dialog = new MessageDialog(message);
-            await dialog.ShowAsync();
-        }
-    }
+			if (result.IsValid(NotificationSeverity.Warning) && result.File != null)
+			{
+				message = $"You picked file: {result.File.DisplayName}";
+			}
+			else
+			{
+				message = "No File Picked :(";
+			}
+			var dialog = new MessageDialog(message);
+			dialog.ShowAsync();
+		}
+	}
 }
--- Wibci.LogicCommand.Tests/NotificationTests.cs
+++ Wibci.LogicCommand.Tests/NotificationTests.cs
@@ -1,51 +1,39 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Wibci.LogicCommand.Tests
 {
-    [TestFixture]
-    public class NotificationTests
-    {
+	[TestFixture]
+	public class NotificationTests
+	{
+		private Notification _notification;
 
-        private Notification _notification;
+		[SetUp]
+		public void TestSetup()
+		{
+			_notification = new Notification();
+		}
 
-        [SetUp]
-        public void TestSetup()
-        {
-            _notification = new Notification();
-        }
+		[Test]
+		public void ToString_Call_ReturnsErrorSummary()
+		{
+			const string error1 = "test";
+			const string error2 = "another";
+			_notification.Add(error1);
+			_notification.Add(error2);
 
-        [Test]
-        public void ToString_Call_ReturnsErrorSummary()
-        {
-            const string error1 = "test";
-            const string error2 = "another";
-            _notification.Add(error1);
-            _notification.Add(error2);
+			string summary = _notification.ToString();
 
-            string summary = _notification.ToString();
+			Assert.IsTrue(summary.Contains(error1), "Expected error 1 to be in the error summary");
+			Assert.IsTrue(summary.Contains(error2), "Expected error 2 to be in the error summary");
+		}
 
-            Assert.IsTrue(summary.Contains(error1), "Expected error 1 to be in the error summary");
-            Assert.IsTrue(summary.Contains(error2), "Expected error 2 to be in the error summary");
+		[Test]
+		public void Severity_GetError_ReturnsCorrectSeverity()
+		{
+			_notification.Add("Info", NotificationSeverity.Info);
+			_notification.Add("Error", NotificationSeverity.Error);
 
-        }
-
-        [Test]        
-        public void Severity_GetError_ReturnsCorrectSeverity()
-        {
-
-            _notification.Add("Info", NotificationSeverity.Info);
-            _notification.Add("Error", NotificationSeverity.Error);
-
-            Assert.AreEqual(NotificationSeverity.Error, _notification.Severity, "Expected Notification Severity to reflect correctly");
-
-
-        }
-
-
-    }
+			Assert.AreEqual(NotificationSeverity.Error, _notification.Severity, "Expected Notification Severity to reflect correctly");
+		}
+	}
 }
